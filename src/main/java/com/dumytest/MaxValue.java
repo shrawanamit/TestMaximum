04dac0ebce00a873e7963c183fb8bda37184dddd
shrawanamit <amitkumar06111@gmail.com>
2020-05-23 02:59:37 +0530

    Added code to print max to std out using generic

--- src/main/java/com/dumytest/MaxValue.java
+++ src/main/java/com/dumytest/MaxValue.java
@@ -13,11 +13,16 @@ public class MaxValue<T extends Comparable<? super T> > {
     }
 
 
-    public static<T extends Comparable> T validateMax(ArrayList<T> list){
+    public <T extends Comparable> T validateMax(ArrayList<T> list){
         T max= list.get(0);
-        for(T element:list)
-            max=(element.compareTo(max)>0)?element:max;
+        for(T element:list) {
+            max = (element.compareTo(max) > 0) ? element : max;
+        }
+        printGenerics(max);
         return max;
+    }
+    private <T extends Comparable> void printGenerics(T max) {
+        System.out.println("Maximum of "+ this.list +" is :"+max);
     }
 
 }
